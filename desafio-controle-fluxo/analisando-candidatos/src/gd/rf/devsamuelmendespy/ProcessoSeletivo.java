0c094a20c453750daf595e64bb24d779ab70e1ca
samuelmendespy <75790706+samuelmendespy@users.noreply.github.com>
2024-11-14 15:12:53 -0300

    feat: add menu interativo

--- desafio-controle-fluxo/analisando-candidatos/src/gd/rf/devsamuelmendespy/ProcessoSeletivo.java
+++ desafio-controle-fluxo/analisando-candidatos/src/gd/rf/devsamuelmendespy/ProcessoSeletivo.java
@@ -10,6 +10,8 @@
 package gd.rf.devsamuelmendespy;
 
 
+import java.util.Scanner;
+
 import java.util.Random;
 
 import java.util.concurrent.ThreadLocalRandom;
@@ -17,38 +19,86 @@ import java.util.concurrent.ThreadLocalRandom;
 
 
 public class ProcessoSeletivo {
+	
+	private static String[] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
+	
+	private static String[] selecionados = new String[5];
+	
+	private static double salarioBase = 2000.0;
+	
+	private static boolean candidatosEscolhidos = false;
 
 	public static void main(String[] args) {
 		
 		
-		// Case 1
-		System.out.println("Processo seletivo");
+		Scanner scanner = new Scanner(System.in);
+		int opcao;
+		
+		do {
+			System.out.println("\nEscolha uma opcao:");
+			System.out.println("1 - Case 1 - Controle de Fluxo");
+			System.out.println("2 - Case 2 - Selecionar Candidatos");
+			System.out.println("3 - Case 3 - Visualizar Selecionados");
+			System.out.println("4 - Case 4 - Contatar Candidatos");
+			
+			opcao = scanner.nextInt();
+			
+			switch (opcao) {
+				case 1:
+					adicionarLinhas();
+					terminalSimularCandidatos();
+					break;
+				case 2:
+					adicionarLinhas();
+					selecionarCandidatos();
+					break;
+				case 3:
+					adicionarLinhas();
+					imprimirSelecionados();
+					break;
+				case 4:
+					adicionarLinhas();
+					terminalContatarCandidatos();
+					break;
+				case 5:
+					System.out.println("Encerrando programa");
+				default:
+					System.out.println("Opcao invalida! Tente novamente.");
+					
+			}
+		}	while (opcao != 5);
+		
+		scanner.close();
+			
+	}
+	
+	static void adicionarLinhas	() {
+	    for (int i = 0; i < 10; i++) {
+	        System.out.println();  // Imprime 10 linhas em branco
+	    }
+	}
+
+	
+	static void terminalSimularCandidatos() {
+		System.out.println("CASE 1");
+		System.out.println("Vamos imaginar que em um processo seletivo existe o valor base");
+		System.out.println("salarial de R$ 2.000,00 e o salário pretendido pelo candidato.");
+		System.out.println("Vamos elaborar um controle de fluxo onde:");
+		System.out.println("- Se o valor salário base for maior que valor salário pretendido, imprima `LIGAR PARA O CANDIDATO`.");
+		System.out.println("- Senão, se o valor salário base base for igual ao valor salário pretendido, imprima `LIGAR PARA O CANDIDATO COM CONTRAPROPOSTA`.");
+		System.out.println("");
+		System.out.println("Demonstração:");
+		System.out.println("");
 		analisarCandidato(1900.0);
 		analisarCandidato(2200.0);
 		analisarCandidato(2000.0);
-		
-		// Case 4
-		String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
-		for(String candidato: candidatos) {
-			entrandoEmContato(candidato);
-		}
-		
 	}
 	
-	/*******************************************************
-	 *  Case 1
-	 *  Vamos imaginar que em um processo seletivo existe o valor base
-	 *  salarial de R$ 2.000,00 e o salário pretendido pelo candidato.
-	 *  Vamos elaborar um controle de fluxo onde:
-	 *  - Se o valor salário base for maior que valor salário pretendido,
-	 *  imprima "LIGAR PARA O CANDIDATO".
-	 *  - Senão, se o valor salário base base for igual ao valor salário
-	 *  pretendido, imprima "LIGAR PARA O CANDIDATO COM CONTRAPROPOSTA".
-	 *
-	 *******************************************************/
 	
 	static void analisarCandidato(double salarioPretendido) {
-		double salarioBase = 2000.00;
+		
+		candidatosEscolhidos = false; // Definir o Processo Seletivo dos Candidatos como ainda não realizado
+		
 		if (salarioBase > salarioPretendido) {
 			System.out.println("LIGAR PARA O CANDIDATO");
 		} else if (salarioBase == salarioPretendido) {
@@ -58,32 +108,39 @@ public class ProcessoSeletivo {
 		}
 	}
 	
-	/*******************************************************
-	 *  Case 2
-	 *  Foi solicitado que nosso sistema garanta que diante das inúmeras
-	 *  candidaturas sejam selecionados apenas no máximo 5 canddiatos para
-	 *  entrevista onde o salário pretendido seja menor ou igual ao salário base.
-	 *
-	 *******************************************************/
 	
 	static void selecionarCandidatos() {
-		// Array com a lista de candidatos
-		String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO","MÔNICA","FABRÍCIO","MIRELA","DANIELA","JORGE"};
 		
-		int candidatosSelecionados = 0;
-		int candidatoAtual = 0;
-		double salarioBase = 2000.0;
-		while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length ) {
-			String candidato = candidatos[candidatoAtual];
-			double salarioPretendido = valorPretendido();
-			System.out.println("O candidato " + candidato + " solicitou este valor de salário: " + salarioPretendido);
+		if (candidatosEscolhidos == false) {
+			System.out.println("CASE 2");
+			System.out.println("Foi solicitado que nosso sistema garanta que diante das inúmeras");
+			System.out.println("candidaturas sejam selecionados apenas no máximo 5 canddiatos para");
+			System.out.println("entrevista onde o salário pretendido seja menor ou igual ao salário base.");
+			System.out.println("");
+			System.out.println("Demonstração:");
+			System.out.println("");
 			
-			if (salarioBase > salarioPretendido) {
-				System.out.println("O candidato "+ candidato + " foi selecionado para a vaga.");
-				candidatosSelecionados++;
-			}
-			candidatoAtual++;
+			int candidatosSelecionados = 0;
+			int candidatoAtual = 0;
+
+			while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length ) {
+				String candidato = candidatos[candidatoAtual];
+				double salarioPretendido = valorPretendido();
+				System.out.println("O candidato " + candidato + " solicitou este valor de salário: R$ " + String.format("%.2f", salarioPretendido) );
+				
+				if (salarioBase > salarioPretendido) {
+					System.out.println("O candidato "+ candidato + " foi selecionado para a vaga.");
+					selecionados[candidatosSelecionados] = candidato;
+					candidatosSelecionados++;
+				}
+				candidatoAtual++;
+				
+			}		
 			
+			candidatosEscolhidos = true; // Definir o Processo Seletivo dos Candidatos como realizado
+			
+		} else {
+			System.out.println("Alerta: O Processo Seletivo já selecionou os candidatos!");
 		}
 		
 	}
@@ -93,44 +150,54 @@ public class ProcessoSeletivo {
 		return ThreadLocalRandom.current().nextDouble(1800, 2200);
 	}
 	
-	/*******************************************************
-	 *  Case 3
-	 *  
-	 *  Agora é hora de imprimir a lista dos candidatos selecionados para
-	 *  disponibilizar para o RH entrar em contato.
-	 *
-	 *******************************************************/
-	
 	static void imprimirSelecionados() {
-		String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
 		
-		System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
-		for(int indice=0; indice < candidatos.length; indice++) {
-			System.out.println("O candidato de nº " + (indice+1) + " é " + candidatos[indice]);
-		}
+		System.out.println("CASE 3");
+		System.out.println("Agora é hora de imprimir a lista dos candidatos selecionados para\n disponibilizar para o RH entrar em contato.");
+		System.out.println("");
+		System.out.println("Demonstração:");
+		System.out.println("");
 		
-		System.out.println("Forma abreviada de interação for each");
-		
-		for(String candidato: candidatos) {
-			System.out.println("O candidato selecionado foi "+ candidato);
+		if (candidatosEscolhidos == true) {
+			System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
+			for (int indice = 0; indice < selecionados.length; indice++) {
+				System.out.println("O candidato de nº " + (indice + 1) + " é " + selecionados[indice]);
+			}
 			
-		}
+			System.out.println(); // Linha para melhorar exibição
+			
+			System.out.println("Forma abreviada de interação for each");
+			
+			for (String candidato: selecionados) {
+				System.out.println("O candidato selecionado foi "+ candidato);
+				
+			}
+		} else {
+			System.out.println("Falha: O Processo Seletivo precisa selecionar os candidatos. ");		
+		}	
 	}
 	
 	
-	/*******************************************************
-	 *  Case 4
-	 *  
-	 *  O RH deverá realizar uma ligação com no máximo 03 tentativas para
-	 *  cada candidato selecionado e caso o candidato atenda, deve-se
-	 *  imprimir:
-	 *  - "CONSEGUIMOS CONTATO COM [ CANDIATO ] APÓS [ TENTATIVAS ] "
-	 *  
-	 *  Do contrário imprima:
-	 *  - "NÃO CONSEGUIMOS CONTATO COM O(A) CANDIDATO(A) [CANDIDATO]".
-	 *
-	 *******************************************************/
-	
+	static void terminalContatarCandidatos() {
+		System.out.println("CASE 4");
+		System.out.println("O RH deverá realizar uma ligação com no máximo 03 tentativas para \n cada candidato selecionado e caso o candidato atenda, deve-se\n imprimir:");
+		System.out.println("`CONSEGUIMOS CONTATO COM [ CANDIATO ] APÓS [ TENTATIVAS ]`");
+		System.out.println("Do contrário imprima:");
+		System.out.println("- `NÃO CONSEGUIMOS CONTATO COM O(A) CANDIDATO(A) [CANDIDATO]`");
+		System.out.println("");
+		System.out.println("Demonstração:");
+		System.out.println("");
+		
+		
+		if (candidatosEscolhidos == true) {
+			for (String candidato: selecionados) {
+				entrandoEmContato(candidato);
+				}
+		} else {
+			System.out.println("Falha: O Processo Seletivo precisa selecionar os candidatos. ");		
+		}
+		
+		}
 
     static void entrandoEmContato(String candidato) {
         int tentativasRealizadas = 1;
@@ -140,14 +207,14 @@ public class ProcessoSeletivo {
         do {
             atendeu = atender();
             continuaTentando = !atendeu;
-            if(continuaTentando) {
+            if (continuaTentando) {
                 tentativasRealizadas++;
-            }else {
+            } else {
                 System.out.println("Contato realizado com sucesso!");
             }
         } while(continuaTentando && tentativasRealizadas < 3);
 
-        if(atendeu) {
+        if (atendeu) {
             System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " APÓS " + tentativasRealizadas + " TENTATIVAS!");
         } else {
             System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + " COM " + tentativasRealizadas + " TENTATIVAS!");
